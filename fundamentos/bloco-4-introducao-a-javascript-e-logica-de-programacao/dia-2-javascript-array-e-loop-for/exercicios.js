fec7bf7ad852c74bbdd5bcb6d657379455eb1035
jatobabispo <dev.jatoba@gmail.com>
2021-11-09 15:22:22 -0300

    atividade 1 2 e 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -0,0 +1,27 @@
+//Lista de Exerciocios do dia 4.2
+
+//Atividade 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (i = 0; i < numbers.length ; i+=1) {
+    console.log(numbers[i]);
+}
+
+//Atividade 2
+
+let soma = 0;
+
+for (i = 0; i < numbers.length; i += 1) {
+        soma += numbers[i];
+}
+console.log("A soma das variaveis é: "+soma);
+
+//Atividade 3
+
+let media = soma / numbers.length;
+
+console.log("A média dos valores é: "+media);
+
+//Atividade 4
+

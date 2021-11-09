8f87110254849793735e597b2a81003853594efe
jatobabispo <dev.jatoba@gmail.com>
2021-11-09 15:52:48 -0300

    atividade 4,5 e 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -25,3 +25,29 @@ console.log("A média dos valores é: "+media);
 
 //Atividade 4
 
+if (media >= 20) {
+    console.log("valor maior que 20");
+} else {
+    console.log("valor menor que 20");
+}
+
+//Atividade 5
+
+let maiorValor = 0;
+for (let i = 0; i < numbers.length; i+=1) {    
+   if ( maiorValor < numbers[i]){
+     maiorValor = numbers[i];
+    }    
+}
+console.log("Maior valor é: "+maiorValor);
+
+//Atividade 6
+
+let impares =[];
+
+for (let i = 0; i < numbers.length; i++) {
+    if ( numbers[i] % 2 == 1 ){
+    impares.push(numbers[i]);
+    }
+}
+console.log("Se houver numeros impares são: "+impares);

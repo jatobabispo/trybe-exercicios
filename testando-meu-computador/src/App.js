e7fb633f8df68947d5008d5deea210ee7a42c6c6
jatobabispo <dev.jatoba@gmail.com>
2022-01-23 00:34:31 -0300

    atividade 1 e 2 ok

---
+++ testando-meu-computador/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const ttt = 'Hello, world!';
+  return (
+    <div >
+      <p>{ttt}</p>
+    </div>
+  );
+}
+
+export default App;
